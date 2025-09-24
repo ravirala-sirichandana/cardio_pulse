740d9d24a3b4989020dd01be3308ffbd3c47d1d2
ravirala-sirichandana <sirichandanaravirala3@gmail.com>
2025-09-24 22:48:42 +0530

    first

--- CARDIO_PULSE.py
+++ CARDIO_PULSE.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib as mbt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 # -------------------------------
